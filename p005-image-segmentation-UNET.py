64d11c4c02653e650d15287f7115695f20c1092c
juanvalen15 <juanvalen15@gmail.com>
2022-09-26 22:39:56 +0200

    updated code

--- p005-image-segmentation-UNET.py
+++ p005-image-segmentation-UNET.py
@@ -65,3 +65,115 @@ plt.imshow(np.reshape(y_train[image_number], (256, 256)), cmap='gray')
 plt.show()        
         
         
+###############################################################
+IMG_HEIGHT = image_dataset.shape[1]
+IMG_WIDTH  = image_dataset.shape[2]
+IMG_CHANNELS = image_dataset.shape[3]
+
+def get_model():
+    return simple_unet_model(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+
+model = get_model()
+
+#If starting with pre-trained weights. 
+#model.load_weights('mitochondria_test.hdf5')
+
+history = model.fit(X_train, y_train, 
+                    batch_size = 16, 
+                    verbose=1, 
+                    epochs=50, 
+                    validation_data=(X_test, y_test), 
+                    shuffle=False)
+
+model.save('mitochondria_test.hdf5')
+
+############################################################
+#Evaluate the model
+
+
+	# evaluate model
+_, acc = model.evaluate(X_test, y_test)
+print("Accuracy = ", (acc * 100.0), "%")
+
+
+#plot the training and validation accuracy and loss at each epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+plt.plot(epochs, acc, 'y', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+##################################
+# Rationale: accuracy not the right performance metric. 
+# If an image background is 90% of the image. Accuracy will be saying that
+# it is accurate to distinguish the backgroun but not the actual important region.
+
+#IOU
+y_pred=model.predict(X_test)
+y_pred_thresholded = y_pred > 0.2
+
+intersection = np.logical_and(y_test, y_pred_thresholded)
+union = np.logical_or(y_test, y_pred_thresholded)
+iou_score = np.sum(intersection) / np.sum(union)
+print("IoU socre is: ", iou_score)
+
+#######################################################################
+#Predict on a few images
+model = get_model()
+model.load_weights('mitochondria_test.hdf5')
+
+test_img_number = random.randint(0, len(X_test))
+test_img = X_test[test_img_number]
+ground_truth=y_test[test_img_number]
+test_img_norm=test_img[:,:,0][:,:,None]
+test_img_input=np.expand_dims(test_img_norm, 0)
+prediction = (model.predict(test_img_input)[0,:,:,0] > 0.2).astype(np.uint8)
+
+test_img_other = cv2.imread('images/largeDS_generatePatches/TESTimages/test_img0.tif', 0)
+#test_img_other = cv2.imread('data/test_images/img8.tif', 0)
+test_img_other_norm = np.expand_dims(normalize(np.array(test_img_other), axis=1),2)
+test_img_other_norm=test_img_other_norm[:,:,0][:,:,None]
+test_img_other_input=np.expand_dims(test_img_other_norm, 0)
+
+#Predict and threshold for values above 0.5 probability
+#Change the probability threshold to low value (e.g. 0.05) for watershed demo.
+prediction_other = (model.predict(test_img_other_input)[0,:,:,0] > 0.2).astype(np.uint8)
+
+plt.figure(figsize=(16, 8))
+plt.subplot(231)
+plt.title('Testing Image')
+plt.imshow(test_img[:,:,0], cmap='gray')
+plt.subplot(232)
+plt.title('Testing Label')
+plt.imshow(ground_truth[:,:,0], cmap='gray')
+plt.subplot(233)
+plt.title('Prediction on test image')
+plt.imshow(prediction, cmap='gray')
+plt.subplot(234)
+plt.title('External Image')
+plt.imshow(test_img_other, cmap='gray')
+plt.subplot(235)
+plt.title('Prediction of external Image')
+plt.imshow(prediction_other, cmap='gray')
+plt.show()
+
+
+
+
+
--- split_TIF_files.py
+++ split_TIF_files.py
@@ -8,25 +8,50 @@ Created on Wed Sep 14 22:16:42 2022
 
 from PIL import Image
 
-img = Image.open('images/training.tif')
-msk = Image.open('images/training_groundtruth.tif')
+# img = Image.open('images/training.tif')
+# msk = Image.open('images/training_groundtruth.tif')
 
+testImg = Image.open('images/testing.tif')
+testMsk = Image.open('images/testing_groundtruth.tif')
 
+# #for i in range(4):
+# i = 0
+# while True:
+#     try:
+#         img.seek(i)
+#         img.save('images/largeDS_generatePatches/images/img%s.tif'%(i,))
+#         i = i + 1
+#     except EOFError:
+#         break
+    
+# i = 0
+# while True:
+#     try:
+#         msk.seek(i)
+#         msk.save('images/largeDS_generatePatches/masks/mask%s.tif'%(i,))
+#         i = i + 1
+#     except EOFError:
+#         break    
+    
 #for i in range(4):
 i = 0
 while True:
     try:
-        img.seek(i)
-        img.save('images/largeDS_generatePatches/images/img%s.tif'%(i,))
+        testImg.seek(i)
+        testImg.save('images/largeDS_generatePatches/TESTimages/test_img%s.tif'%(i,))
         i = i + 1
     except EOFError:
         break
     
+#for i in range(4):
 i = 0
 while True:
     try:
-        msk.seek(i)
-        msk.save('images/largeDS_generatePatches/masks/mask%s.tif'%(i,))
+        testMsk.seek(i)
+        testMsk.save('images/largeDS_generatePatches/TESTmasks/test_msk%s.tif'%(i,))
         i = i + 1
     except EOFError:
         break    
+
+
+
