086c906860486865c46bf4f745342a12309a6078
Juan <juanvalen15@gmail.com>
2022-11-14 19:26:30 +0100

    adding p006 for image segmentation of retinal vessels

---
+++ p006-retina-UNET.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 17:16:11 2022
+
+@author: juan
+"""
+
+from simple_unet_model import simple_unet_model   #Use normal unet model
+from keras.utils import normalize
+import os
+import cv2
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+
+image_directory = 'images/largeDS_generatePatches/images/'
+mask_directory =  'images/largeDS_generatePatches/masks/'
+
+
+SIZE = 256
+image_dataset = []  #Many ways to handle data, you can use pandas. Here, we are using a list format.  
+mask_dataset = []  #Place holders to define add labels. We will add 0 to all parasitized images and 1 to uninfected.
+
+images = sorted(os.listdir(image_directory))
+images.pop(0) # to remove .DS_store file
+for i, image_name in enumerate(images):    #Remember enumerate method adds a counter and returns the enumerate object
+    if (image_name.split('.')[1] == 'tif'):
+        #print(image_directory+image_name)
+        image = cv2.imread(image_directory+image_name, 0)
+        image = Image.fromarray(image)
+        image = image.resize((SIZE, SIZE))
+        image_dataset.append(np.array(image))
+
+#Iterate through all images in Uninfected folder, resize to 64 x 64
+#Then save into the same numpy array 'dataset' but with label 1
+
+masks = sorted(os.listdir(mask_directory))
+masks.pop(0) # to remove .DS_store file
+for i, image_name in enumerate(masks):
+    if (image_name.split('.')[1] == 'tif'):
+        image = cv2.imread(mask_directory+image_name, 0)
+        image = Image.fromarray(image)
+        image = image.resize((SIZE, SIZE))
+        mask_dataset.append(np.array(image))        
+        
+#Normalize images
+image_dataset = np.expand_dims(normalize(np.array(image_dataset), axis=1),3)
+#D not normalize masks, just rescale to 0 to 1.
+mask_dataset = np.expand_dims((np.array(mask_dataset)),3) /255.
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(image_dataset, mask_dataset, test_size = 0.10, random_state = 0)
+
+#Sanity check, view few mages
+import random
+import numpy as np
+image_number = random.randint(0, len(X_train))
+plt.figure(figsize=(12, 6))
+plt.subplot(121)
+plt.imshow(np.reshape(X_train[image_number], (256, 256)), cmap='gray')
+plt.subplot(122)
+plt.imshow(np.reshape(y_train[image_number], (256, 256)), cmap='gray')
+plt.show()        
+        
+        
+###############################################################
+IMG_HEIGHT = image_dataset.shape[1]
+IMG_WIDTH  = image_dataset.shape[2]
+IMG_CHANNELS = image_dataset.shape[3]
+
+def get_model():
+    return simple_unet_model(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+
+model = get_model()
+
+#If starting with pre-trained weights. 
+#model.load_weights('mitochondria_test.hdf5')
+
+history = model.fit(X_train, y_train, 
+                    batch_size = 16, 
+                    verbose=1, 
+                    epochs=50, 
+                    validation_data=(X_test, y_test), 
+                    shuffle=False)
+
+model.save('mitochondria_test.hdf5')
+
+############################################################
+#Evaluate the model
+
+
+	# evaluate model
+_, acc = model.evaluate(X_test, y_test)
+print("Accuracy = ", (acc * 100.0), "%")
+
+
+#plot the training and validation accuracy and loss at each epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+plt.plot(epochs, acc, 'y', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+##################################
+# Rationale: accuracy not the right performance metric. 
+# If an image background is 90% of the image. Accuracy will be saying that
+# it is accurate to distinguish the backgroun but not the actual important region.
+
+#IOU
+y_pred=model.predict(X_test)
+y_pred_thresholded = y_pred > 0.2
+
+intersection = np.logical_and(y_test, y_pred_thresholded)
+union = np.logical_or(y_test, y_pred_thresholded)
+iou_score = np.sum(intersection) / np.sum(union)
+print("IoU socre is: ", iou_score)
+
+#######################################################################
+#Predict on a few images
+model = get_model()
+model.load_weights('mitochondria_test.hdf5')
+
+test_img_number = random.randint(0, len(X_test))
+test_img = X_test[test_img_number]
+ground_truth=y_test[test_img_number]
+test_img_norm=test_img[:,:,0][:,:,None]
+test_img_input=np.expand_dims(test_img_norm, 0)
+prediction = (model.predict(test_img_input)[0,:,:,0] > 0.2).astype(np.uint8)
+
+test_img_other = cv2.imread('images/largeDS_generatePatches/TESTimages/test_img100.tif', 0)
+test_img_other = Image.fromarray(test_img_other)
+test_img_other = test_img_other.resize((SIZE, SIZE))
+test_img_other_norm = np.expand_dims(normalize(np.array(test_img_other), axis=1),2)
+test_img_other_norm=test_img_other_norm[:,:,0][:,:,None]
+test_img_other_input=np.expand_dims(test_img_other_norm, 0)
+
+#Predict and threshold for values above 0.5 probability
+#Change the probability threshold to low value (e.g. 0.05) for watershed demo.
+prediction_other = (model.predict(test_img_other_input)[0,:,:,0] > 0.2).astype(np.uint8)
+
+plt.figure(figsize=(16, 8))
+plt.subplot(231)
+plt.title('Testing Image')
+plt.imshow(test_img[:,:,0], cmap='gray')
+plt.subplot(232)
+plt.title('Testing Label')
+plt.imshow(ground_truth[:,:,0], cmap='gray')
+plt.subplot(233)
+plt.title('Prediction on test image')
+plt.imshow(prediction, cmap='gray')
+plt.subplot(234)
+plt.title('External Image')
+plt.imshow(test_img_other, cmap='gray')
+plt.subplot(235)
+plt.title('Prediction of external Image')
+plt.imshow(prediction_other, cmap='gray')
+plt.show()
+
+
+
+
+
